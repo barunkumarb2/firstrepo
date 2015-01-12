07cbf1c1a63842fc7ca62d3c299ba6a4b554ec95
arun-balaji <arun.balaji@a-cti.com>
2015-01-12 15:36:28 -0800

    first logger commit

--- src/com/test/TestController.java
+++ src/com/test/TestController.java
@@ -1,16 +1,28 @@
 package com.test;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+
 @Controller
 public class TestController {
 
+	private static final Logger	mLogger	= Logger.getLogger(TestController.class.getPackage().getName());
+	
+	
+	public TestController(){
+		mLogger.info("Constructor Invoked");
+	}
+	
 	@RequestMapping(value="introtest",method={RequestMethod.POST,RequestMethod.GET})
 	public String showHomePage(){
 		
-	 System.out.println("Invocked");
+		mLogger.info("Invoked");
+	
 		
 		return "Success";
 	}
--- war/Success.jsp
+++ war/Success.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-Welcome to Test Git Repository
+Welcome to Test Git Repository Test project
 </body>
 </html>
